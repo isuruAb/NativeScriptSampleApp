b7ee12f500dab9164f9589f08d4a6a1755207334
isuruab <vpisabeywardana@gmail.com>
2017-04-20 18:15:07 +0530

    first commit

---
+++ platforms/ios/build/Groceries.build/Debug-iphonesimulator/Groceries.build/Objects-normal/i386/main.d
@@ -0,0 +1,9 @@
+dependencies: \
+  /Users/macbook/Documents/nativeScript/Groceries/platforms/ios/Groceries/Groceries-Prefix.pch \
+  /Users/macbook/Documents/nativeScript/Groceries/platforms/ios/internal/main.m \
+  /Users/macbook/Documents/nativeScript/Groceries/platforms/ios/internal/NativeScript.framework/Headers/NativeScript.h \
+  /Users/macbook/Documents/nativeScript/Groceries/platforms/ios/internal/NativeScript.framework/Headers/TNSRuntime+Diagnostics.h \
+  /Users/macbook/Documents/nativeScript/Groceries/platforms/ios/internal/NativeScript.framework/Headers/TNSRuntime.h \
+  /Users/macbook/Documents/nativeScript/Groceries/platforms/ios/internal/NativeScript.framework/Headers/TNSRuntime+Inspector.h \
+  /Users/macbook/Documents/nativeScript/Groceries/platforms/ios/internal/TNSExceptionHandler.h \
+  /Users/macbook/Documents/nativeScript/Groceries/platforms/ios/internal/TNSDebugging.h
